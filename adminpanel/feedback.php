abe745a713429a12baef4520bd35cdb32b14e8e6
Beka <40782830+beketovekb@users.noreply.github.com>
2022-03-28 03:13:49 +0600

    beka

--- adminpanel/feedback.php
+++ adminpanel/feedback.php
@@ -23,9 +23,9 @@ foreach ($titles as $title) {
         <form action="../include/save.php" method="POST">
             <div class="recentOrders">
                 <div class="cardHeader">
-                    <h2>О нас</h2>
+                    <h2>Обратная связь</h2>
                     <input type="submit" value="Save" class="btn" />
-                    <input type="text" name="fun" style="width: 100%; display: none; " value="about">
+                    <input type="text" name="fun" style="width: 100%; display: none; " value="feedback">
                 </div>
                 <table>
                     <thead>
@@ -39,12 +39,42 @@ foreach ($titles as $title) {
                         <!--Russian-->
                         <tr>
                             <td>Заголовок</td>
-                            <td><input type="text" name="r4" style="width: 100%;" value="<?php print($ftitler['6']); ?>"></td>
+                            <td><input type="text" name="r6" style="width: 100%;" value="<?php print($ftitler['6']); ?>"></td>
                             <td>RU</td>
                         </tr>
                         <tr>
                             <td>Описание</td>
-                            <td><textarea name="r5" id="" style="width: 100%;" cols="40" rows="10"><?php print($ftitler['7']); ?></textarea></td>
+                            <td><textarea name="r7" id="" style="width: 100%;" cols="40" rows="10"><?php print($ftitler['7']); ?></textarea></td>
+                            <td>RU</td>
+                        </tr>
+                        <tr>
+                            <td>Заголовок</td>
+                            <td><input type="text" name="r24" style="width: 100%;" value="<?php print($ftitler['24']); ?>"></td>
+                            <td>RU</td>
+                        </tr>
+                        <tr>
+                            <td>Описание</td>
+                            <td><input type="text" name="r25" style="width: 100%;" value="<?php print($ftitler['25']); ?>"></td>
+                            <td>RU</td>
+                        </tr>
+                        <tr>
+                            <td>Имя</td>
+                            <td><input type="text" name="r26" style="width: 100%;" value="<?php print($ftitler['26']); ?>"></td>
+                            <td>RU</td>
+                        </tr>
+                        <tr>
+                            <td>Номер</td>
+                            <td><input type="text" name="r27" style="width: 100%;" value="<?php print($ftitler['27']); ?>"></td>
+                            <td>RU</td>
+                        </tr>
+                        <tr>
+                            <td>Кнопка</td>
+                            <td><input type="text" name="r28" style="width: 100%;" value="<?php print($ftitler['28']); ?>"></td>
+                            <td>RU</td>
+                        </tr>
+                        <tr>
+                            <td>Согласие</td>
+                            <td><input type="text" name="r29" style="width: 100%;" value="<?php print($ftitler['29']); ?>"></td>
                             <td>RU</td>
                         </tr>
                     </tbody>
@@ -59,12 +89,42 @@ foreach ($titles as $title) {
                         <!--Englis-->
                         <tr>
                             <td>Заголовок</td>
-                            <td><input type="text" name="e4" style="width: 100%;" value="<?php print($ftitlee['6']); ?>"></td>
+                            <td><input type="text" name="e6" style="width: 100%;" value="<?php print($ftitlee['6']); ?>"></td>
                             <td>ENG</td>
                         </tr>
                         <tr>
                             <td>Описание</td>
-                            <td><textarea name="e5" id="" style="width: 100%;" cols="40" rows="10"><?php print($ftitlee['7']); ?></textarea></td>
+                            <td><textarea name="e7" id="" style="width: 100%;" cols="40" rows="10"><?php print($ftitlee['7']); ?></textarea></td>
+                            <td>ENG</td>
+                        </tr>
+                        <tr>
+                            <td>Заголовок</td>
+                            <td><input type="text" name="e24" style="width: 100%;" value="<?php print($ftitlee['24']); ?>"></td>
+                            <td>ENG</td>
+                        </tr>
+                        <tr>
+                            <td>Описание</td>
+                            <td><input type="text" name="e25" style="width: 100%;" value="<?php print($ftitlee['25']); ?>"></td>
+                            <td>ENG</td>
+                        </tr>
+                        <tr>
+                            <td>Имя</td>
+                            <td><input type="text" name="e26" style="width: 100%;" value="<?php print($ftitlee['26']); ?>"></td>
+                            <td>ENG</td>
+                        </tr>
+                        <tr>
+                            <td>Номер</td>
+                            <td><input type="text" name="e27" style="width: 100%;" value="<?php print($ftitlee['27']); ?>"></td>
+                            <td>ENG</td>
+                        </tr>
+                        <tr>
+                            <td>Кнопка</td>
+                            <td><input type="text" name="e28" style="width: 100%;" value="<?php print($ftitlee['28']); ?>"></td>
+                            <td>ENG</td>
+                        </tr>
+                        <tr>
+                            <td>Согласие</td>
+                            <td><input type="text" name="e29" style="width: 100%;" value="<?php print($ftitlee['29']); ?>"></td>
                             <td>ENG</td>
                         </tr>
                         </tbody>
@@ -79,12 +139,42 @@ foreach ($titles as $title) {
                         <!-- Kazakh-->
                         <tr>
                             <td>Заголовок</td>
-                            <td><input type="text" name="k4" style="width: 100%;" value="<?php print($ftitlek['6']); ?>"></td>
+                            <td><input type="text" name="k6" style="width: 100%;" value="<?php print($ftitlek['6']); ?>"></td>
                             <td>KZ</td>
                         </tr>
                         <tr>
                             <td>Описание</td>
-                            <td><textarea name="k5" id="" style="width: 100%;" cols="40" rows="10"><?php print($ftitlek['7']); ?></textarea></td>
+                            <td><textarea name="k7" id="" style="width: 100%;" cols="40" rows="10"><?php print($ftitlek['7']); ?></textarea></td>
+                            <td>KZ</td>
+                        </tr>
+                        <tr>
+                            <td>Заголовок</td>
+                            <td><input type="text" name="k24" style="width: 100%;" value="<?php print($ftitlek['24']); ?>"></td>
+                            <td>KZ</td>
+                        </tr>
+                        <tr>
+                            <td>Описание</td>
+                            <td><input type="text" name="k25" style="width: 100%;" value="<?php print($ftitlek['25']); ?>"></td>
+                            <td>KZ</td>
+                        </tr>
+                        <tr>
+                            <td>Имя</td>
+                            <td><input type="text" name="k26" style="width: 100%;" value="<?php print($ftitlek['26']); ?>"></td>
+                            <td>KZ</td>
+                        </tr>
+                        <tr>
+                            <td>Номер</td>
+                            <td><input type="text" name="k27" style="width: 100%;" value="<?php print($ftitlek['27']); ?>"></td>
+                            <td>KZ</td>
+                        </tr>
+                        <tr>
+                            <td>Кнопка</td>
+                            <td><input type="text" name="k28" style="width: 100%;" value="<?php print($ftitlek['28']); ?>"></td>
+                            <td>KZ</td>
+                        </tr>
+                        <tr>
+                            <td>Согласие</td>
+                            <td><input type="text" name="k29" style="width: 100%;" value="<?php print($ftitlek['29']); ?>"></td>
                             <td>KZ</td>
                         </tr>
                     </tbody>
