fa47a68b6c1c00d8a35e99384d3cc26b38d66b64
Beka <40782830+beketovekb@users.noreply.github.com>
2022-03-28 19:02:11 +0600

    beka

--- include/database.php
+++ include/database.php
@@ -1,8 +1,12 @@
 <?php
+// $host = 'localhost';
+// $login='root';
+// $password = 'root';
+// $name_bd= 'rrm_bd';
 $host = 'localhost';
-$login='root';
-$password = 'root';
-$name_bd= 'rrm_bd';
+$login='p-18653_beka';
+$password = '341833beka';
+$name_bd= 'p-18653_rrm';
 $link = mysqli_connect($host,$login,$password,$name_bd);
 
 if(mysqli_connect_errno())
