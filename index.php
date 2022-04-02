6e96592cc41b65bb7d53cd3bae3dd2bdb1dd9f18
skimaksss1 <skimaksss@gmail.com>
2022-04-02 13:07:41 +0600

    Skimaksss

--- index.php
+++ index.php
@@ -56,7 +56,7 @@ $partners = get_partners($link);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <!-- <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"> -->
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.css" />
     <link type="image/x-icon" rel="shortcut icon" href="img/favicon.ico">
