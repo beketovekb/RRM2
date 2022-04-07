bdafbbff1ddfd66b5e12243202e291c1706c9297
skimaksss1 <skimaksss@gmail.com>
2022-04-07 15:40:45 +0600

    Skimaksss

--- adminpanel/editProject.php
+++ adminpanel/editProject.php
@@ -17,7 +17,6 @@ foreach ($editions as $edition) {
 }
 $imgs = get_img_project($link, $pos);
 ?>
-<title>ТЕСТ</title>
 <div class="main">
     <?php include "menu.php"; ?>
     <div class="details">
